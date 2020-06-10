65b2066b464ca2c7805c657e02869ac1524e3e1b
nozakifamima <saginogi@gmail.com>
2020-06-10 21:01:46 +0900

    [finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '496227032bdb5b29ade93c90561687b9cbd69557d3049a9856b2eb347ba9b5e2a5fa81c469ea47f8d76e4d7c310de1cf499b8b2b98854b638dc632252709a555'
